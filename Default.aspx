3477faeedd6456d3f124e299fc5b860e3fe98333
ranadheer.k.2451@gmail.com <ranadheer.k.2451@gmail.com>
2022-02-10 15:01:45 +0530

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FirstWebApp.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>First Web App</title>
+</head>
+<body>
+
+        <form id="form1" runat="server">
+        <div style="margin-top: 0px">
+                <asp:TextBox ID="txtContext" runat="server"></asp:TextBox>
+                <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+        </div>
+
+            <div>
+            </div>
+            <asp:ListBox ID="ListBox1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged">
+                <asp:ListItem>Theme1</asp:ListItem>
+                <asp:ListItem>Theme2</asp:ListItem>
+            </asp:ListBox>
+        </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FirstWebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        static string themeValue;
+        protected void Page_PreInit(Object Sender, EventArgs e)
+        {
+            Page.Theme = themeValue;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("First ASP Page");
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            var textValue = txtContext.Text;
+            Response.Write("<script>alert('" + textValue + "')</script>");
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            themeValue = ListBox1.SelectedItem.Value;
+            Response.Redirect(Request.Url.ToString());
+        }
+    }
+}
